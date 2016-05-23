7718d1cc7402b0a0cec242178202c3751449d9bc
Craig Eisen <Ceisen91@gmail.com>
2016-05-23 19:38:50 -0400

    updated read me and code comments

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 var express = require('express');
 var app = express();
 
+//The welcome screen with directions how to formulate HTTP requests to be fizzbuzz-ed
+//This page will show when the user visits http://localhost:3001
 app.get('/', function(req,res){
-  res.send('hello')
+  res.send("Hello, welcome to this RESTful fizzbuzz app. Please type an HTTP request of the following structure: </br></br> http://localhost:3001/api?    word= <b>word</b> &    max_value= <b>number</b> </br></br> Where the <b>word</b> is either <i>fizz</i>, <i>buzz</i>, or <i>fizzbuzz</i> and the <b>number</b> is any integer 1 or greater.")
 });
 
+//The meat of the application. Will take the query submitted within the HTTP request and formulate the output as described in the directions to this exercise
 app.get('/api', function(req,res){
+  //maxVal grabs the max value that is submitted via the HTTP request
   var maxVal = req.query.max_value;
+  //the default status is error unless the HTTP request is coded correctly
   var status = 'error';
+  //the default array of numbers is empty unless the HTTP request is coded correctly
   var numbers = [];
+  //the following if statements parse thru the word being queried (either fizz, buzz, or fizzbuzz) and formulates the output accordingly
   if(req.query.word == 'fizzbuzz'){
     status = 'ok';
+    //this loop will fill the numbers array with the correct values as pertaining to the rules of fizzbuzz
     for(var i = 1; i <= maxVal; i++){
+      //fizzbuzz must be divisible by 3 and 5
       if(i%5 == 0 && i%3 ==0){
         numbers.push(i);
       }
     }
+    //send the formulated response as JSON to the browser
     res.send({'status': status, 'numbers': numbers}).json;
   }
   else if(req.query.word == 'fizz'){
     status = 'ok';
     for(var i = 1; i <= maxVal; i++){
+      //fizz must be divisible by 3
       if(i%3 == 0){
         numbers.push(i);
       }
@@ -30,6 +41,7 @@ app.get('/api', function(req,res){
   else if(req.query.word == 'buzz'){
     status = 'ok';
     for(var i = 1; i <= maxVal; i++){
+      //buzz must be divisible by 5
       if(i%5 == 0){
         numbers.push(i);
       }
@@ -37,10 +49,12 @@ app.get('/api', function(req,res){
     res.send({'status': status, 'numbers': numbers}).json;
   }
   else {
+    //if the HTTP request is coded incorrectly make the response status to 400 and send an error status to the browser
     res.status(400).json({ 'status': status, 'numbers': numbers})
   }
 });
 
+//the port that the server is listening on
 app.listen(3001, function(){
   console.log('welcome')
 })
---
+++ readme.md
@@ -0,0 +1,18 @@
+The target system for this coding exercise is Mac.
+This application has been written using javascript and leverages the third-party dependencies given by Node.js/Express.js.
+
+
+Directions:
+
+1) After downloading the zipfile with the source code and opening the file, go to the computer's terminal and navigate into the newly unzipped file
+2) Type the command $npm install
+3) Open the browser and visit http://localhost:3001
+4) Formulate further HTTP requests for fizzbuzz as outlined by the information displayed on the browser and also given in the directions to this coding exercise. A few examples of correct requests are as follows:
+  a) http://localhost:3001/api?word=fizz&max_value=150
+  b) http://localhost:3001/api?word=buzz&max_value=30
+  c) http://localhost:3001/api?word=fizzbuzz&max_value=100
+
+And a few examples of incorrect requests are as follows:
+  a) http://localhost:3001/api?word=notfizzorbuzz&max_value=150
+  b) http://localhost:3001/api?word=fizz
+  c) http://localhost:3001/api?max_value=150
